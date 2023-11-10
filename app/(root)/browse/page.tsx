ae03637268b9e1984ec5883188dc1250fb849670
Mirabbosbek9778 <jwicj969@gmail.com>
2023-11-10 07:17:37 +0500

    auth login Github env clientID clientSecret full

--- app/(root)/browse/page.tsx
+++ app/(root)/browse/page.tsx
@@ -2,12 +2,17 @@
 
 import Login from "@/components/shared/login";
 import { useGlobalContext } from "@/context";
+import { useSession } from "next-auth/react";
 import React from "react";
 
 const Page = () => {
   const { account } = useGlobalContext();
+  const { data: session } = useSession();
+  console.log(session);
 
-  if (account === null) return <Login />;
+  if (session === null) return <Login />;
+  // if (account === null) return <Login />;
+
   return <div>hello browse</div>;
 };
 
--- components/shared/login.tsx
+++ components/shared/login.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import { Button } from "../ui/button";
+import { AiFillGithub } from "react-icons/ai";
+import { signIn } from "next-auth/react";
 
 const Login = () => {
   return (
-    <div>
-      Login componets
+    <div className={"w-full h-screen"}>
+      <div className={"absolute inset-0"}>
+        <Image
+          src={
+            "https://repository-images.githubusercontent.com/299409710/b42f7780-0fe1-11eb-8460-e459acd20fb4"
+          }
+          alt={"bg"}
+          fill
+        />
+        <div
+          className={
+            "relative z-10 w-[500px] bg-black/50  h-[50vh] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-2xl px-8 py-4"
+          }
+        >
+          <div className={"flex items-center flex-col"}>
+            <h1 className={"text-4xl font-bold"}>Login</h1>
+            <Button
+              onClick={() => signIn("github")}
+              className={
+                "mt-4 flex items-center gap-2 w-full h-[56px] bg-red-500 !text-white hove:bg-red-800"
+              }
+            >
+              <AiFillGithub className={"w-7 h-7"} />
+              Sign in with GitHub
+            </Button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
