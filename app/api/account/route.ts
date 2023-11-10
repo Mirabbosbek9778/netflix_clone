6f08456b74d2b111a89f2bd9738acf08a5bd96b0
Mirabbosbek9778 <jwicj969@gmail.com>
2023-11-11 01:18:24 +0500

    api ready full

--- app/api/account/route.ts
+++ app/api/account/route.ts
@@ -5,6 +5,8 @@ import { hash } from "bcryptjs";
 
 export const dynamic = "force-dynamic";
 
+// create a new account
+
 export async function POST(req: Request) {
   try {
     await connectToDatabase();
@@ -31,5 +33,59 @@ export async function POST(req: Request) {
     return NextResponse.json({ account });
   } catch (e) {
     return NextResponse.json({ success: false, message: "hato" });
+  }
+}
+
+// Get all accounts
+
+export async function GET(req: Request) {
+  try {
+    await connectToDatabase();
+    const { searchParams } = new URL(req.url);
+    const uid = searchParams.get("uid");
+
+    if (!uid) {
+      return NextResponse.json({
+        success: false,
+        message: "Account id is mandatory",
+      });
+    }
+    const accounts = await Account.find({ uid });
+
+    return NextResponse.json({ success: true, accounts });
+  } catch (e) {
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+}
+
+// delete
+
+export async function DELETE(req: Request) {
+  try {
+    await connectToDatabase();
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get("id");
+
+    if (!id) {
+      return NextResponse.json({
+        success: false,
+        message: "Account id is mandatory",
+      });
+    }
+
+    const account = await Account.findByIdAndDelete(id);
+
+    return NextResponse.json({
+      success: true,
+      message: "Account ochirildi",
+    });
+  } catch (e) {
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong",
+    });
   }
 }
